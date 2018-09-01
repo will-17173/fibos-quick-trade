350a95f3a9f5e047fba86c22a4a557bee38b8849
will-17173 <huangzhaojian@gmail.com>
2018-09-01 23:18:38 +0800

    eosin

--- eosin.js
+++ eosin.js
@@ -2,8 +2,11 @@ const client = require('./client');
 const amount = process.argv[2];
 
 if(!/^[0-9]+\.[0-9]{4}$/.test(amount)){
-    console.error('请输入转帐数值');
+    console.error('请输入正确的转帐数值，精确到小数点后4位');
     return;
 }
 var res = client.transferEOS(amount + " EOS");
 console.log(res)
+if(res.broadcast){
+    console.log('EOS主链转帐到FIBOS成功')
+}
